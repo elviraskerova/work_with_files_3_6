7ab8c850392baa9c5f09409b43a658a9c80f72b6
Elvira <elviraskerova@gmail.com>
2023-01-07 22:55:04 +0300

    refresh

---
+++ test_file_zip.py
@@ -0,0 +1,18 @@
+from os.path import basename
+from zipfile import ZipFile
+import os
+
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
+
+def test_create_archive():
+    files = os.listdir(path)
+    with ZipFile('resources/info.zip', 'w') as zip_file:
+        for file in files:
+            file_name = os.path.join(path, file)
+            zip_file.write(file_name, basename(file_name))
+
+
+
+
+
+
