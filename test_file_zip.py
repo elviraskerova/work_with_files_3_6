a1ac4731302276cc4f2bcf162e3dea2485ea00e9
Elvira <elviraskerova@gmail.com>
2023-01-10 21:11:56 +0300

    refresh 2

--- test_file_zip.py
+++ test_file_zip.py
@@ -1,10 +1,13 @@
+import zipfile
 from os.path import basename
 from zipfile import ZipFile
 import os
-import csv
+import glob
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
 
+
+ # создание зип
 def test_create_archive():
     files = os.listdir(path)
     with ZipFile('resources/info.zip', 'w') as zip_file:
@@ -12,6 +15,45 @@ def test_create_archive():
             file_name = os.path.join(path, file)
             zip_file.write(file_name, basename(file_name))
 
+#распаковка зип
+# extract_dir = 'extract_dir'
+#
+# with zipfile.ZipFile('resources/info.zip') as zf:
+#     zf.extractall(extract_dir)
+#
+# for file in glob.glob(extract_dir + '/**', recursive=True):
+#     print(file)
+
+
+    # Чтение и проверка pdf
+def test_read_pdf():
+    my_zipfile = ZipFile(path)
+    with zipfile.ZipFile(path) as zf:
+        r_pdf = my_zipfile.extract('PDF_file.pdf')
+        reader1 = PdfReader(r_pdf)
+        text = reader1.pages[0].extract_text()
+        print(text)
+        assert 'Пример pdf' in text
+        os.remove(r_pdf)
+
+
+    # Чтение и проверка csv
+def test_read_csv():
+    my_zipfile = ZipFile(path)
+    text = str(my_zipfile.read('csv_file.csv'))
+    print(text)
+    assert 'Hello World' in text
+
+
+    # Чтение и проверка xlsx
+def test_read_xlsx():
+    my_zipfile = ZipFile(path)
+    workbook = my_zipfile.extract('xlsx_file.xlsx')
+    xlsx_book = load_workbook(workbook)
+    sheet = xlsx_book.active
+    print(sheet.cell(row=1, column=1).value)
+    assert 'Desktop' in sheet.cell(row=1, column=1).value
+    os.remove(workbook)
 
 
 
